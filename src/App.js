d225061eb4bb3b6cccec53a07d892918adf121f6
Luiz Antonio <antonioluizgutierres@gmail.com>
2023-02-10 14:46:17 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import api from'./sistema cep/api';
+import { useState } from 'react';
+import './edicao.css';
+import { FiCheck } from 'react-icons/fi';
 
 function App() {
+
+const [input, setInput] = useState('')
+const {cep, setCep} = useState({});
+
+async function procuracep(){
+  if(input === ''){
+    alert("Insira um CEP!!!")
+    return;
+  }
+
+  try{
+    const response = await api.get(`${input}/json`);
+    setCep(response.data);
+    setInput("");
+  }catch{
+    alert("Ops algo errado ocorreu")
+    setInput("")
+  }
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="titulo">Buscador CEP</h1>
+
+      <div className="containerinput">
+       <input
+       type="text"
+       placeholder="Digite seu cep..."
+       value={input}
+       onChange={(e) => setInput(e.target.value)}
+       />
+
+<button className="botaoprocurar" onClick={procuracep}>
+    <FiCheck size={25} color="#000"/>
+</button>
+      </div>
+
+      
+    {Object.keys(cep).length > 0 && (
+      <main className="main">
+        <h2> CEP: {cep.cep}</h2>
+        <span>{cep.logradouro}</span>
+        <span>Complemento:{cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf}</span>
+        
+    </main>
+    )}
     </div>
   );
+
 }
 
 export default App;
